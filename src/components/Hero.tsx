e48e6d1f50410ea983b30c09551fbb6ea9ae820c
mooncityorg <cryptomoon116@gmail.com>
2024-07-16 00:28:46 +0900

    fix:title, link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -11,12 +11,12 @@ const Hero: FC = () => {
     <section className="min-h-[800px] 2xl:min-h-[calc(100vh-40px)] relative pb-[120px] z-20">
       <div className="flex items-center justify-between gap-2 lg:gap-6 px-28 relative z-20">
         <div className="flex">
-          <Link href={"https://x.com/onepugclub"} target="_blank">
+          <Link href={"https://x.com/"} target="_blank">
             <div className="w-9 lg:w-[60px] h-9 lg:h-[60px] hover:scale-110 grid place-content-center m-8">
               <TwitterIcon />
             </div>
           </Link>
-          <Link href={"https://t.me/puggyclub"} target="_blank">
+          <Link href={"https://t.me/"} target="_blank">
             <div className="w-9 lg:w-[60px] h-9 lg:h-[60px] hover:scale-110 grid place-content-center m-8">
               <TelegramIcon />
             </div>
